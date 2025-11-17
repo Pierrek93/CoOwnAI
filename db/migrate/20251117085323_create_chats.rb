b6401b753d1956b59659ab8cc271484072df3420
Pierre Keane <Pierrekeane@gmail.com>
2025-11-17 18:00:51 +0800

    created the models, still need to do validation and associations.

---
+++ db/migrate/20251117085323_create_chats.rb
@@ -0,0 +1,12 @@
+class CreateChats < ActiveRecord::Migration[7.1]
+  def change
+    create_table :chats do |t|
+      t.string :title
+      t.integer :characters
+      t.references :user, null: false, foreign_key: true
+      t.references :section, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
