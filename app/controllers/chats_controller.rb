da48fadeaf3ca628b04ce1d19c9ca1ed4df483ca
Andreas Sousa Branca <andreaslsb@gmail.com>
2025-11-18 17:44:37 +0800

    adapted creation of chat page, not finalized

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,10 @@
 class ChatsController < ApplicationController
+
+  def show
+    @chat = Chat.find(params[:id])
+    @user = @chat.user
+    @section = @chat.section
+    @message = Message.new()
+  end
+
 end
