22b9e0585f146a86035c685d250173cd2a87682f
Andreas Sousa Branca <andreaslsb@gmail.com>
2025-11-18 17:47:53 +0800

    updated migration to include content in messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_17_085423) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_18_094723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_17_085423) do
     t.bigint "chat_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
